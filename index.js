046c308e5af59f13e0e92e15f26a83aef8a6e88e
rafsan3503 <rafsanchowdhuryrudro@gmail.com>
2022-11-08 05:54:45 +0600

    single api data

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -16,18 +16,43 @@ app.get("/", (req, res) => {
 // mongodb connect
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nk3n7xe.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log("database connected");
-  client.close();
-});
+
+async function run() {
+  try {
+    const serviceCollection = client.db("footea").collection("services");
+
+    // get services
+    app.get("/services", async (req, res) => {
+      const size = parseInt(req.query.size);
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      if (size) {
+        const services = await cursor.limit(size).toArray();
+        res.send(services);
+        return;
+      }
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    // get single service
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+      console.log(id);
+    });
+  } finally {
+  }
+}
+
+run().catch((err) => console.log(err));
 
 app.listen(port, () => {
   console.log(`Server is running from port ${port}`);
