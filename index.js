6cd6f74990a01cda2460ee57e259451779b10121
rafsan3503 <rafsanchowdhuryrudro@gmail.com>
2022-11-08 04:45:08 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +13,22 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+// mongodb connect
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nk3n7xe.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("database connected");
+  client.close();
+});
+
 app.listen(port, () => {
-  console.log(`Server is running from port${port}`);
+  console.log(`Server is running from port ${port}`);
 });
